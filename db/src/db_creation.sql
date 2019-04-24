bd938fede587492dc3010fe1087b6330454886eb
Nadir Benallal <brofmaster@gmail.com>
2019-04-24 15:18:22 +0200

    First version of the db creation script

---
+++ db/src/db_creation.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE IF NOT EXISTS lifenodes DEFAULT CHARACTER SET utf8;
+
+USE lifenodes;
+
+CREATE TABLE IF NOT EXISTS events (
+    eventID INT(4),
+    eventDate DATETIME NOT NULL,
+    eventName VARCHAR(255) NOT NULL,
+    eventDescription VARCHAR(1000),
+
+    PRIMARY KEY (eventID)
+);
